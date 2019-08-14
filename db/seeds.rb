92bb1e8b8cc4c1e5e7c776a9867d74c3fba39c32
Enrique <hhfolte@gmail.com>
2019-08-14 21:08:15 +0200

    add options to app

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,36 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+# restaurants = Restaurant.create([{ name: 'Yono' }, { address: 'Berlin' }, { phone_number: '123456789' }, { category: "chinese"} ])
+
+# restaurants = Restaurant.create([{ name: 'Molo' }, { address: 'Wien' }, { phone_number: '11111111' }, { category: "chinese"} ])
+
+# restaurants = Restaurant.create([{ name: 'Yolz' }, { address: 'Stuttgart' }, { phone_number: '2222222' }, { category: "chinese"} ])
+
+# restaurants = Restaurant.create([{ name: 'Zola' }, { address: 'Paris' }, { phone_number: '333333' }, { category: "chinese"} ])
+
+# restaurants = Restaurant.create([{ name: 'Fola' }, { address: 'London' }, { phone_number: '44444' }, { category: "chinese"} ])
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:        "chinese",
+    phone_number:   "111111"
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:        "chinese",
+    phone_number:   "23232"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts "Finished! #{Restaurant.all.count}"
